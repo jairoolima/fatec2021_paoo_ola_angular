5c65ef9f5fcae7579d00aa16d416712bd1f5086c
Jairo Lima <jairo.olima@gmail.com>
2021-02-25 23:29:32 -0300

    Terminamos a apostila01, testamos a criacao de componentes manualmente e com CLI e instalamos o CLI 4.6

---
+++ src/app/ola-angular2/ola-angular2.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core'
+
+@Component({
+    selector: "app-ola-angular2", 
+    templateUrl: "./ola-angular2.component.html",
+    styleUrls: ['./ola-angular2.component.css']
+})
+export class OlaAngular2Component {
+    framework = "Angular"
+    // aux = {{"Angular"}}
+}
